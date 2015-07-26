5a2f0c20fc9991b08cf01c7cce4252b9359f85ca
JonsGitHub <jonathancarter101@gmail.com>
2015-07-25 17:36:46 -0700

    Background Color Changer

--- Background Color Changer/ViewController.swift
+++ Background Color Changer/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet var mySwitch: UISwitch!
+    @IBOutlet var Label: UILabel!
+    // Variables above, functions below
+    
+    @IBAction func ToggleRed(sender: AnyObject) {
+        if mySwitch.on
+        {
+            Label.text = "Switch is pressed!"
+        }
+        else
+        {
+            Label.text = "Use the switch to change the text"
+        }
+    }
+    
+    
+    @IBAction func ChangeRed(sender: UIButton) {
+        self.view.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func ChangeWhite(sender: UIButton) {
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
